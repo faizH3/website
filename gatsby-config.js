a5bb696bca4ce310c1ab9f5b69ed72a087745317
faizH3 <20420026@student.binadarma.ac.id>
2021-06-12 01:17:56 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,31 @@
-module.exports = ({
-  basePath = "/",
-  contentPath = "content/",
-  showThemeLogo = true,
-  theme = "classic",
-}) => {
-  return {
-    siteMetadata: {
-      description: "Personal page of John Doe",
-      locale: "en",
-      showThemeLogo,
-      title: "John Doe",
-      formspreeEndpoint: "https://formspree.io/f/{your-id}",
+module.exports = {
+  siteMetadata: {
+    title: 'Name Surname - Portfolio'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
     },
-    plugins: [
-      {
-        resolve: `gatsby-plugin-postcss`,
-        options: {
-          postCssPlugins: [
-            require("tailwindcss")(require("./tailwind.config")(theme)),
-            require("postcss-input-range"),
-            require("autoprefixer"),
-          ],
-        },
-      },
-      `gatsby-plugin-react-helmet`,
-      `gatsby-transformer-yaml`,
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: contentPath,
-        },
-      },
-      `gatsby-plugin-react-svg`,
-      `gatsby-plugin-image`,
-      `gatsby-transformer-sharp`,
-      `gatsby-plugin-sharp`,
-    ],
-  }
-}
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#f6f1ed',
+        theme_color: '#f6f1ed',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png' // This path is relative to the root of the site.
+      }
+    }
+    // 'gatsby-plugin-offline',
+  ]
+};
